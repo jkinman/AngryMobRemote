918ab22ae7de7481374aafe07afd1295dbf1ab08
Joel Kinman <joel.kinman@gmail.com>
2022-11-01 17:19:01 -0700

    rtc connection made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import * as peerjs from './modules/WebRTCPeer.js'
 
 function App() {
+
+  const conneciton = peerjs.connect("9d64a6e7-767f-4e35-9ee3-fb866f3b4b5d")
+  console.log( conneciton)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
